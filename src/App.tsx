73819b005ea6277c6b202132e9ccbb7644890775
nayyaung9 <naygit.developer@gmail.com>
2022-12-29 21:35:14 +0630

    feat: implement geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ApplicationNavigator from '~navigation/ApplicationNavigator';
+
 import ThemeManager from '~utils/styles/ThemeManager';
-import log from '~startup/log';
+
+import log from '~utils/startup/log';
+import geolocation from '~utils/startup/geolocation';
 
 const App: React.FC = () => {
   log('log', 'App', 'Rendering App');
+
+  useEffect(() => {
+    const mountBoot = async () => {
+      try {
+        const geolocationResponse = await geolocation();
+        console.log("geolocationResponse", geolocationResponse)
+      } catch {}
+    }
+
+    mountBoot();
+  }, []);
+
   return (
     <ThemeManager>
       <ApplicationNavigator />
