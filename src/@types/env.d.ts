e6657cc3b82a8b371fbbf392346029641ca8c7a0
nayyaung9 <naygit.developer@gmail.com>
2023-01-07 22:48:51 +0630

    fix: solve env module

---
+++ src/@types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const ENVIRONMENT: string;
+  export const ENDPOINT_SECRET_KEY: string;
+  export const GEOCODER_ENDPOINT: string;
+  export const GEOCODER_KEY: string;
+}
