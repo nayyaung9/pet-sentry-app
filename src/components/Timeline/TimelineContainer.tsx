f397ed779da327c08646bb28bea33be7b8ee8393
nayyaung9 <naygit.developer@gmail.com>
2023-01-20 21:28:18 +0630

    feat: testing react native animated spinkit

--- src/components/Timeline/TimelineContainer.tsx
+++ src/components/Timeline/TimelineContainer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {StyleSheet, FlatList} from 'react-native';
+import {StyleSheet, FlatList, View} from 'react-native';
 import PinMyPet from '~components/PinMyPet';
 import {useTimelineQuery} from '~utils/queryHooks/timeline';
 import {StyleConstants} from '~utils/styles/constants';
 import {useTheme} from '~utils/styles/ThemeManager';
 import TimelineCard from './TimelineCard';
+import {Flow} from 'react-native-animated-spinkit';
 
 const TimelineContainer = ({queryKey}: {queryKey: string}) => {
   const {isLoading, data, error} = useTimelineQuery({
@@ -14,16 +15,22 @@ const TimelineContainer = ({queryKey}: {queryKey: string}) => {
   const {colors} = useTheme();
   return (
     <>
-      {!isLoading && data && (
-        <FlatList
-          ListHeaderComponent={<PinMyPet />}
-          ListHeaderComponentStyle={styles.listHeaderView}
-          stickyHeaderIndices={[0]}
-          showsVerticalScrollIndicator={false}
-          style={{flex: 1, backgroundColor: colors.background}}
-          data={data}
-          renderItem={({item, index}) => <TimelineCard {...{item, index}} />}
-        />
+      {isLoading ? (
+        <View style={styles.loadingContainer}>
+          <Flow size={48} color={colors.primary} />
+        </View>
+      ) : (
+        data && (
+          <FlatList
+            ListHeaderComponent={<PinMyPet />}
+            ListHeaderComponentStyle={styles.listHeaderView}
+            stickyHeaderIndices={[0]}
+            showsVerticalScrollIndicator={false}
+            style={{flex: 1, backgroundColor: colors.background}}
+            data={data}
+            renderItem={({item, index}) => <TimelineCard {...{item, index}} />}
+          />
+        )
       )}
     </>
   );
@@ -36,5 +43,10 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: '#f0f2f5',
   },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
 export default TimelineContainer;
